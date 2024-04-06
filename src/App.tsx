979220b0a7d3a683a73d1ba57605f2c87e3d1619
QuangDuy212 <duy2k4ml1234@gmail.com>
2024-04-06 16:31:38 +0700

    video31

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { useSelector } from 'react-redux'
-import { RootState } from './redux/store'
+import Header from './components/header';
+import TabsContent from './components/tabs.content';
 
 function App() {
-  const count = useSelector((state: RootState) => state.counter);
-  console.log(">>> check count: ", count);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <div>
-        My current count = {count.value}
-        <div>
-          <button>Increase + 1</button>
-        </div>
-      </div>
+      <Header />
+      <TabsContent />
     </>
   )
 }
